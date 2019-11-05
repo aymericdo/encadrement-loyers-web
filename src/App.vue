e8b0a9d68909f9c0271ed46985e162c4eb53aade
Lucien Landanger <lucienlandanger@Macbook-Lucieng.local>
2019-11-05 13:38:07 +0100

    landing ok, need responsive

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,147 @@
 <template>
   <div id="app">
-    <TopBar />
-    <Body />
+    <div class="centerWrapper">
+      <Hero />
+      <SectionWhy />
+      <SectionWhere />
+      <SectionHow />
+      <SectionDisclaimer />
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
-import Body from "./components/Body.vue";
-import TopBar from "./components/TopBar.vue";
+import Hero from "./components/Hero.vue";
+//import Body from "./components/Body.vue";
+import SectionWhy from "./components/SectionWhy.vue";
+import SectionWhere from "./components/SectionWhere.vue";
+import SectionHow from "./components/SectionHow.vue";
+import SectionDisclaimer from "./components/SectionDisclaimer.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "app",
   components: {
-    Body,
-    TopBar
+    Hero,
+    SectionWhy,
+    SectionWhere,
+    SectionHow,
+    SectionDisclaimer,
+    Footer
   }
 };
 </script>
 
 <style>
+/* Webfont: Garnett-Black */@font-face {
+    font-family: 'Garnett';
+    src: url('./assets/fonts/Garnett-Medium.eot'); /* IE9 Compat Modes */
+    src: url('./assets/fonts/Garnett-Medium.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('./assets/fonts/Garnett-Medium.woff') format('woff'), /* Modern Browsers */
+         url('./assets/fonts/Garnett-Medium.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('./assets/fonts/Garnett-Medium.svg#Garnett-Medium') format('svg'); /* Legacy iOS */
+    font-style: normal;
+    font-weight: 500;
+    text-rendering: optimizeLegibility;
+}
+
+/* Webfont: Garnett-Bold */@font-face {
+    font-family: 'Garnett';
+    src: url('./assets/fonts/Garnett-Bold.eot'); /* IE9 Compat Modes */
+    src: url('./assets/fonts/Garnett-Bold.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('./assets/fonts/Garnett-Bold.woff') format('woff'), /* Modern Browsers */
+         url('./assets/fonts/Garnett-Bold.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('./assets/fonts/Garnett-Bold.svg#Garnett-Bold') format('svg'); /* Legacy iOS */
+    font-style: normal;
+    font-weight: 700;
+    text-rendering: optimizeLegibility;
+}
+
+/* Webfont: Garnett-Regular */@font-face {
+    font-family: 'Garnett';
+    src: url('./assets/fonts/Garnett-Regular.eot'); /* IE9 Compat Modes */
+    src: url('./assets/fonts/Garnett-Regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('./assets/fonts/Garnett-Regular.woff') format('woff'), /* Modern Browsers */
+         url('./assets/fonts/Garnett-Regular.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('./assets/fonts/Garnett-Regular.svg#Garnett-Regular') format('svg'); /* Legacy iOS */
+    font-style: normal;
+    font-weight: 400;
+    text-rendering: optimizeLegibility;
+}
+
+/* Webfont: Garnett-Regular */@font-face {
+    font-family: 'Garnett';
+    src: url('./assets/fonts/Garnett-SemiBold.eot'); /* IE9 Compat Modes */
+    src: url('./assets/fonts/Garnett-SemiBold.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('./assets/fonts/Garnett-SemiBold.woff') format('woff'), /* Modern Browsers */
+         url('./assets/fonts/Garnett-SemiBold.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('./assets/fonts/Garnett-SemiBold.svg#Garnett-SemiBold') format('svg'); /* Legacy iOS */
+    font-style: normal;
+    font-weight: 600;
+    text-rendering: optimizeLegibility;
+}
+button:focus{
+  outline-style:none!important;
+  box-shadow:none!important;
+  border-color:transparent;
+}
+
+html, body{
+  background-color: #050505;
+}
+
+ul li{
+  list-style: none;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Garnett","Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #9E9E9E;
+  display: flex;
+  flex-direction: row;
+  background-color: #050505;
+  width: 100vw;
+  justify-content: center;
+  font-size: 22px;
+  letter-spacing: -0.23px;
+  line-height: 34px;
 }
+
+.sectionTitle h3{
+font-weight: 700;
+font-size: 38px;
+color: #F7F7F5;
+letter-spacing: -0.31px;
+line-height: 52px;
+}
+
+.centerWrapper{
+  display: flex;
+  flex-direction: column;
+  max-width: 856px;
+  padding-top: calc((100vw - 856px) / 3);
+  padding-bottom: calc((100vw - 856px) / 3);
+
+}
+
+.section{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin: 160px 0 0 0;
+}
+
+.sectionTitle{
+  max-width: 248px;
+  width: 100%;
+}
+
+.rightPart{
+  margin-left: 56px;
+  max-width: 552px;
+  width: 100%;
+}
+
 </style>
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,67 +1,58 @@
 <template>
   <div itemscope itemtype="http://schema.org/Thing" class="description">
-    <div class="col-sm-12 col-md-8">
-      <h2 itemprop="name">Une extension pour vous aider dans votre recherche de location</h2>
+    <p itemprop="description">
+      Ceci a pour but d'informer, et d'avertir les potentiels futurs locataires
+      que certains loyers parisiens ne respectent pas l'encadrement des loyers, en vigueur depuis le 01/07/2019.
+    </p>
 
-      <p itemprop="description">
-        Cette extension permet de savoir si le prix de votre loyer (ou futur loyer) est
-        <i>potentiellement</i> légal ou non.
-      </p>
+    <h4>Une extension disponible sur les sites les plus populaires</h4>
+    <ul>
+      <li itemprop="url">seloger.com</li>
+      <li itemprop="url">leboncoin.fr</li>
+      <li itemprop="url">loueragile.fr</li>
+      <li itemprop="url">pap.fr</li>
+      <li itemprop="url">logic-immo.com</li>
+      <li itemprop="url">immobilier.lefigaro.fr</li>
+      <li itemprop="url">orpi.com</li>
+    </ul>
 
-      <p itemprop="description">
-        Ceci a pour but d'informer, et d'avertir les potentiels futurs locataires
-        que certains loyers parisiens ne respectent pas l'encadrement des loyers, en vigueur depuis le 01/07/2019.
-      </p>
+    <h4>Une extension simple d'utilisation</h4>
+    <p itemprop="description">
+      Lorsque vous naviguez sur les annonces de location de l'un de ces sites,
+      l'extension vous donne automatiquement le prix le plus haut que vous devriez payer
+      selon les critères trouvés sur la page du logement. Ensuite, vous pouvez accéder à plus d'informations en cliquant sur
+      la petite icône de l'extension, à droite de la barre d'URL.
+    </p>
 
-      <h4>Une extension disponible sur les sites les plus populaires</h4>
-      <ul>
-        <li itemprop="url">seloger.com</li>
-        <li itemprop="url">leboncoin.fr</li>
-        <li itemprop="url">loueragile.fr</li>
-        <li itemprop="url">pap.fr</li>
-        <li itemprop="url">logic-immo.com</li>
-        <li itemprop="url">immobilier.lefigaro.fr</li>
-        <li itemprop="url">orpi.com</li>
-      </ul>
-
-      <h4>Une extension simple d'utilisation</h4>
-      <p itemprop="description">
-        Lorsque vous naviguez sur les annonces de location de l'un de ces sites,
-        l'extension vous donne automatiquement le prix le plus haut que vous devriez payer
-        selon les critères trouvés sur la page du logement. Ensuite, vous pouvez accéder à plus d'informations en cliquant sur
-        la petite icône de l'extension, à droite de la barre d'URL.
-      </p>
-
-      <h4>Disponible sur Firefox et Chrome</h4>
-      <ul>
-        <li>
-          Chrome
-          <a
-            itemprop="url"
-            href="https://chrome.google.com/webstore/detail/encadrement-loyers-paris/nkmghhgmpbngkbjnhpomddeglkpkkfeo?hl=fr"
-          >Lien vers le chrome webstore</a>
-        </li>
-        <li>
-          Firefox
-          <a
-            itemprop="url"
-            href="https://addons.mozilla.org/fr/firefox/addon/encadrement-loyers-paris/"
-          >Lien vers le mozilla add-ons store</a>
-        </li>
-      </ul>
-
-      <h5>Avertissement</h5>
-      <p
-        itemprop="description"
-      >Selon la mairie de Paris, il est possible de dépasser le prix de l'encadrement sous certaines conditions très strictes (garage, balcon, prestations extraordinaires, etc). Les annonces notées "illégales" ne le sont donc peut-être pas. (Ceci est un petit ajout pour ne pas choquer les âmes sensibles de nos chers propriétaires parisiens)</p>
-      <p itemprop="description">
-        Pour plus d'informations, vous pouvez vous renseigner ici
+    <h4>Disponible sur Firefox et Chrome</h4>
+    <ul>
+      <li>
+        Chrome
         <a
           itemprop="url"
-          href="https://www.paris.fr/pages/l-encadrement-des-loyers-parisiens-en-vigueur-le-1er-aout-2712"
-        >https://www.paris.fr/pages/l-encadrement-des-loyers-parisiens-en-vigueur-le-1er-aout-2712</a>
-      </p>
-    </div>
+          href="https://chrome.google.com/webstore/detail/encadrement-loyers-paris/nkmghhgmpbngkbjnhpomddeglkpkkfeo?hl=fr"
+        >Lien vers le chrome webstore</a>
+      </li>
+      <li>
+        Firefox
+        <a
+          itemprop="url"
+          href="https://addons.mozilla.org/fr/firefox/addon/encadrement-loyers-paris/"
+        >Lien vers le mozilla add-ons store</a>
+      </li>
+    </ul>
+
+    <h5>Avertissement</h5>
+    <p
+      itemprop="description"
+    >Selon la mairie de Paris, il est possible de dépasser le prix de l'encadrement sous certaines conditions très strictes (garage, balcon, prestations extraordinaires, etc). Les annonces notées "illégales" ne le sont donc peut-être pas. (Ceci est un petit ajout pour ne pas choquer les âmes sensibles de nos chers propriétaires parisiens)</p>
+    <p itemprop="description">
+      Pour plus d'informations, vous pouvez vous renseigner ici
+      <a
+        itemprop="url"
+        href="https://www.paris.fr/pages/l-encadrement-des-loyers-parisiens-en-vigueur-le-1er-aout-2712"
+      >https://www.paris.fr/pages/l-encadrement-des-loyers-parisiens-en-vigueur-le-1er-aout-2712</a>
+    </p>
   </div>
 </template>
 
@@ -72,12 +63,5 @@ export default {
 };
 </script>
 
-<style scoped>
-.description {
-  padding: 40px;
-}
-
-p {
-  text-align: justify;
-}
+<style>
 </style>
