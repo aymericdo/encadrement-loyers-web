093a56383a4c37fc4d765a8c3a295bd48c724ab6
Thomas Legrand <legrand.thomas5@hotmail.fr>
2019-11-07 22:47:35 +0100

    add map a la route stats

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="stats">
-    <h1>Rendez l'argent please</h1>
+    <div id="map"></div>
   </div>
 </template>
+
+<script>
+import vegaEmbed from "vega-embed";
+
+export default {
+  name: "stats",
+  mounted: function() {
+    fetch('http://localhost:3000/stats/map')
+        .then(res => res.json())
+        .then(spec => {
+          vegaEmbed('#map', spec);
+    })
+  } 
+};
+
+</script>
