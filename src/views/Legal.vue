3f918fd710c1e791e0dd9e74cefef08bb49c35fb
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-02-06 11:26:49 +0100

    add legal mention

--- src/views/Legal.vue
+++ src/views/Legal.vue
@@ -2,12 +2,17 @@
   <div id="legal">
     <transition name="slide-fade" v-on:leave="leave">
       <div v-if="isMounted" class="center-wrapper">
+        <h1>
+          Aucune donnée n'est collectée par l'extension, hormis celle nécessaire au calcul et à la réalisation de quelques
+          <router-link to="/stats">statistiques</router-link>.
+          Aucune donnée utilisateur n'est collectée.
+        </h1>
         <h2>Informations légales</h2>
         <h3>1. Présentation du site.</h3>
         <p>
           En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site
           <a
-            href="https://encadrement-loyers.fr//"
+            href="https://encadrement-loyers.fr/"
           >https://encadrement-loyers.fr/</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
         </p>
         <p>
