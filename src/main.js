f40192429fa7f1dcf2bc180bbeabfaed3f799f8a
aymeric dominique <aymeric.dominique@gmail.com>
2021-04-02 17:02:36 +0200

    teubé

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import router from './router'
 
 const app = createApp(App)
 app.use(router).mount('#app')
-// app.config.globalProperties.$domain = 'https://encadrement-loyers.herokuapp.com/'
-app.config.globalProperties.$domain = 'http://localhost:3000/'
+app.config.globalProperties.$domain = 'https://encadrement-loyers.herokuapp.com/'
+// app.config.globalProperties.$domain = 'http://localhost:3000/'
