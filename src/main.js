c144713bef8a9ea2eea7de4217f0408b582c253f
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-11-09 22:58:48 +0100

    update

--- src/main.js
+++ src/main.js
@@ -4,11 +4,23 @@ import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
 Sentry.init({
   app,
   project: "web",
+  dsn: "https://285198e42e2227ea3fe1615cb98b1e6c@o4507146908139520.ingest.de.sentry.io/4508270551171152",
   environment: process.env.NODE_ENV,
-  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 app.use(router).mount("#app");
