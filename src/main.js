d3a0df7058b0d3a168d27aee5df24137f367c144
Legrand Thomas <legrand.thomas5@hotmail.fr>
2024-11-09 21:49:32 +0100

    Ajout de Sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import * as Sentry from "@sentry/vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+Sentry.init({
+  app,
+  project: "web",
+  environment: process.env.NODE_ENV,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+});
 
 const app = createApp(App);
 app.use(router).mount("#app");
